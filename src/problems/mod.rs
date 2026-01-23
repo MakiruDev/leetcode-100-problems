eee5058ca51c77da6e15c6530f222204f87e311c
MakiruDev <minecraftxbox75893@gmail.com>
2026-01-23 23:51:48 +0700

    feat: CLI scaffolding and LC-4/LC-5 solutions
    * feat(cli): add problem/way scaffolding command
    
    * feat(lc-4): solve arithmetic slices
    
    * feat(lc-5): max consecutive ones iii (way1)

--- src/problems/mod.rs
+++ src/problems/mod.rs
@@ -1,4 +1,6 @@
+pub mod arithmetic_slices;
 pub mod func_run;
 pub mod longest_substring_without_repeating_characters;
 pub mod maximum_average_subarray;
 pub mod repeated_dna_sequences;
+pub mod max_consecutive_ones_iii;
